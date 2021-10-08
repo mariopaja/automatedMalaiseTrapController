f791d83111e7078a4ea195a9a678bb4bb7560041
mariopaja <mpaja15@epika.edu.al>
2021-10-08 13:52:47 +0200

    battery check condition updated

--- batteryChecker.h
+++ batteryChecker.h
@@ -1,6 +1,10 @@
+/*
+* checkBatteryLevel() checks the battery level
+* Sends a SMS notification to the user if the battery is below a certain level
+*/
 void checkBatteryLevel() {
   //135K Ohm & 270K Ohm Resistors are needed
-  if ((analogRead(A3) * (5.00 / 1023.00) * 3) < double(10)) {
-    SMS("Low Battery");
+  if ((analogRead(A3) * (5.00 / 1023.00) * 3) < 10.0) {
+    if (activatedSMS)SMS(createMessage(systemID, "Low Battery"));
   }
 }
