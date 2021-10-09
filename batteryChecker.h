477c27e4f5d4a3597a70f96a182a41438e12c684
mariopaja <mpaja15@epika.edu.al>
2021-10-09 15:28:38 +0200

    batteryChecker.h comments updated

--- batteryChecker.h
+++ batteryChecker.h
@@ -1,9 +1,11 @@
 /*
 * checkBatteryLevel() checks the battery level
 * Sends a SMS notification to the user if the battery is below a certain level
+* 135K Ohm & 270K Ohm Resistors
+* 10.0 is the minimum operation voltage of the system
+* should be a bit higher than the cut off voltage of the solar charge controller
 */
 void checkBatteryLevel() {
-  //135K Ohm & 270K Ohm Resistors are needed
   if ((analogRead(A3) * (5.00 / 1023.00) * 3) < 10.0) {
     if (activatedSMS)SMS(createMessage(systemID, "Low Battery"));
   }
